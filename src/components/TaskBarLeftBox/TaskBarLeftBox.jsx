9e97e413446c8f4b99b4053d094d105fdfc8b49f
Diego <diegosantaolallamartin@gmail.com>
2020-07-21 01:35:40 +0200

    Added translations

--- src/components/TaskBarLeftBox/TaskBarLeftBox.jsx
+++ src/components/TaskBarLeftBox/TaskBarLeftBox.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 import taskbarBarLeftBoxStyle from './taskbarLeftBox.module.scss';
+import { useTranslation } from 'react-i18next';
 
 export default function TaskBarLeftBox() {
-
+  const { t, i18n } = useTranslation('common');
   return (
-    <div className={`taskbar-left ${taskbarBarLeftBoxStyle.taskbarLeftBox}` }> 
-      TASK BAR LEFT BOX
+    <div className={`taskbar-left ${taskbarBarLeftBoxStyle.taskbarLeftBox}`}>
+      {t('taskbar.leftBox.start')}
+
+
+   
     </div>
   )
 }
--- src/components/TaskBarRightBox/TaskBarRightBox.jsx
+++ src/components/TaskBarRightBox/TaskBarRightBox.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import taskbarRightBoxStyles from './taskbarRightBox.module.scss'
+import { useTranslation } from "react-i18next";
 
 export default function TaskBarRightBox() {
+  const [time, setTime] = useState(formatDate(new Date()));
+  const { t, i18n } = useTranslation('common');
+
+  useEffect(() => {
+    let timerID = setInterval(() => {
+      getTime();
+    }, 10000);
+    return () => {
+      clearInterval(timerID);
+    }
+  })
+
+  function getTime() {
+    let time = new Date();
+    time = formatDate(time);
+    setTime(time);
+  }
+
+  function formatDate(date) {
+    return date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+  }
+
   return (
     <div className={`taskbarRightBox ${taskbarRightBoxStyles.taskbarRightBox}`}>
-      TASK BAR RIGHT BOX
+      <button onClick={() => i18n.changeLanguage('es')}>ES</button>
+      <button onClick={() => i18n.changeLanguage('en')}>EN</button>
+      {time.toString()}
     </div>
   )
 }
