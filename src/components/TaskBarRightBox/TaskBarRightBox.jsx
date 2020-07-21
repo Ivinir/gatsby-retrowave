453cf3bf81d56653891b077738fbfcc37b847f4b
Diego <diegosantaolallamartin@gmail.com>
2020-07-21 02:19:52 +0200

    Added clock

--- src/components/TaskBarRightBox/TaskBarRightBox.jsx
+++ src/components/TaskBarRightBox/TaskBarRightBox.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import taskbarRightBoxStyles from './taskbarRightBox.module.scss'
+import styles from './taskbarRightBox.module.scss'
 import { useTranslation } from "react-i18next";
 
-export default function TaskBarRightBox() {
-  const [time, setTime] = useState(formatDate(new Date()));
+const TaskBarRightBox = () => {
+  const [time, setTime] = useState(() => new Date());
   const { t, i18n } = useTranslation('common');
 
   useEffect(() => {
+    getTime();
     let timerID = setInterval(() => {
       getTime();
     }, 10000);
@@ -16,20 +17,17 @@ export default function TaskBarRightBox() {
   })
 
   function getTime() {
-    let time = new Date();
-    time = formatDate(time);
-    setTime(time);
-  }
-
-  function formatDate(date) {
-    return date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    setTime(new Date())
+    setTime(time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }));
   }
 
   return (
-    <div className={`taskbarRightBox ${taskbarRightBoxStyles.taskbarRightBox}`}>
+    <div className={`taskbarRightBox ${styles.taskbarRightBox}`}>
       <button onClick={() => i18n.changeLanguage('es')}>ES</button>
       <button onClick={() => i18n.changeLanguage('en')}>EN</button>
-      {time.toString()}
+      <span className={`taskbarRightBox ${styles.taskbarRightBox__clock}`}>{time.toString()}</span>
     </div>
   )
 }
+
+export default TaskBarRightBox;
